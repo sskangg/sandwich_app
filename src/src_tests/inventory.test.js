2102790463b6236e13ea65d40acdeeb488a1acad
Tyler McGoffin <tyler.mcgoffin@circleci.com>
2021-04-23 14:35:22 -0700

    added basic tests

---
+++ src/src_tests/inventory.test.js
@@ -0,0 +1,9 @@
+
+describe("Basic test for CircleCI testing", () => {
+
+    test("that a new inventory is empty when an Inventory object is instantiated", () => {
+
+        expect(true).toBe(true);
+
+    });
+});
