ddfeac3ab90ee32a771c3fa3183cc3490d1b159a
Tyler McGoffin <tyler.mcgoffin@circleci.com>
2021-04-23 15:51:12 -0700

    updated the test to be a touch more interesting

--- src/src_tests/inventory.test.js
+++ src/src_tests/inventory.test.js
@@ -1,9 +1,18 @@
 
 describe("Basic test for CircleCI testing", () => {
 
-    test("that a new inventory is empty when an Inventory object is instantiated", () => {
 
-        expect(true).toBe(true);
+
+    test("that the meaning of life, the universe, and everything is 42", () => {
+        
+   
+        const theAnswer = new Promise( (resolve, reject) => {
+            setTimeout(() => {
+                resolve(42);
+            }, 2.3652); // *10^17ms ~ 7.5 million years
+        }); 
+
+        return expect(theAnswer).resolves.toBe(42);
 
     });
 });
